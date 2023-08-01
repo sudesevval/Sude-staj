f709a5ac6f88135a2bf9fa40ebc671515e4b5a3a
sudesevval <sudesevvalciloglu@gmail.com>
2023-08-01 14:42:35 +0300

    fonskiyon

--- ornekk.py
+++ ornekk.py
@@ -1,1 +1,8 @@
-print(merhaba)
+def fonksiyon(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+number = int(input("Bir sayı girin: "))
+result = fonksiyon(number)
+print(result)
